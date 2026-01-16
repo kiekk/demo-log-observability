81c928aa651b59df5060a10e73d2ff9e125ff85d
shyoon <shyoon991@gmail.com>
2026-01-17 08:20:15 +0900

    Initial Commit

---
+++ src/main/kotlin/com/example/logobservability/DemoLogObservabilityApplication.kt
@@ -0,0 +1,11 @@
+package com.example.logobservability
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoLogObservabilityApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoLogObservabilityApplication>(*args)
+}
